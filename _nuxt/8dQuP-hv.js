17eba969fa3dcbf850e253220303a2e0b58495b5
abdell1999 <abdellahbukadduh99@gmail.com>
2026-03-05 19:23:32 +0000

    Update from https://github.com/abdell1999/ebook-reader/commit/2c251ce2af394e2708822266099ffa2642a476ec

---
+++ _nuxt/8dQuP-hv.js
@@ -0,0 +1,1 @@
+import"./DJB58ssn.js";const e=""+new URL("pdf.worker.min.B_fnEKel.mjs",import.meta.url).href;export{e as default};
---
+++ _nuxt/BKhZyJj6.js
@@ -0,0 +1,1 @@
+import{b1 as o}from"./DJB58ssn.js";const r=()=>o("color-mode").value;export{r as u};
