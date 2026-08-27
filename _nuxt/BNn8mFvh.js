fa59938118713dd0211ca364e4193d4b3f5fc7e4
abdell1999 <abdellahbukadduh99@gmail.com>
2026-08-27 14:55:49 +0000

    Update from https://github.com/abdell1999/ebook-reader/commit/a3713e5883412fadf46de30fac2e9aafd82a8642

--- _nuxt/BNn8mFvh.js
+++
@@ -1,1 +0,0 @@
-import{a9 as a}from"./Zyjq-yLq.js";const o=e=>{a("/",{replace:!0})};export{o as g};
---
+++ _nuxt/CT-xFfMy.js
@@ -0,0 +1,1 @@
+import"./C0PO4neb.js";const e=""+new URL("pdf.worker.min.B_fnEKel.mjs",import.meta.url).href;export{e as default};
